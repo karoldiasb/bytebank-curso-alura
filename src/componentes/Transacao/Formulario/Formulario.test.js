8ff65b38e7e4dd3ddd719296b628d319c3c5d68d
Karoline Barreto <karolinedbarreto@gmail.com>
2024-03-01 11:36:49 -0300

    feat: add test and describe

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -1,14 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import Formulario from './index';
+import userEvent from '@testing-library/user-event'
 
-test('should render an input field', () => {
-    render(<Formulario />);
-    const textField = screen.getByPlaceholderText('Digite um valor');
-    expect(textField).toBeInTheDocument();
-});
+describe('should render an input field', () => {
+    test(' in the document', () => {
+        render(<Formulario />);
+        const textField = screen.getByPlaceholderText('Digite um valor');
+        expect(textField).toBeInTheDocument();
+    });
 
-test('should render an input field with type number', () => {
-    render(<Formulario />);
-    const textField = screen.getByPlaceholderText('Digite um valor');
-    expect(textField).toHaveAttribute('type', 'number');
-});
+    test(' with type number', () => {
+        render(<Formulario />);
+        const textField = screen.getByPlaceholderText('Digite um valor');
+        expect(textField).toHaveAttribute('type', 'number');
+    });
+
+    test(' that can be filled', () => {
+        render(<Formulario />);
+        const textField = screen.getByPlaceholderText('Digite um valor');
+        userEvent.type(textField, '50')
+        expect(textField).toHaveValue(50);
+    });
+})
